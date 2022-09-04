d41422d53777071a71a6e4950c1aac5e058705cf
NippleAlert <112808477+NippleAlert@users.noreply.github.com>
2022-09-04 21:57:06 +0300

    Update Praktikum.java

--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,6 @@
 public class Praktikum {
   public static void main(String[] args) {
     System.out.println("Не важно, кто напротив. Главное, кто рядом!");
+    System.out.println("Безумно можно быть первыым");
   }
 }
